640935ffbfe7e44c64ed41ab03d39417af106df9
vikiboreca <vikiboreca@gmail.com>
2025-08-22 01:32:34 +0300

    ai have a counter

--- Documents/androidProjects/app/src/main/java/com/example/sofiawidget/Widgets/BaseWidget.java
+++ Documents/androidProjects/app/src/main/java/com/example/sofiawidget/Widgets/BaseWidget.java
@@ -1,8 +1,10 @@
 package com.example.sofiawidget.Widgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.example.sofiawidget.R;
@@ -14,14 +16,17 @@ public class BaseWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+        var views = new RemoteViews(context.getPackageName(), R.layout.base_widget);
+        Intent intent = new Intent(context, BaseWidget.class);
+        intent.setAction("GetData");
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.base_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        var pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+        views.setOnClickPendingIntent(R.id.button_click_me, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+
     }
 
     @Override
@@ -40,5 +45,21 @@ public class BaseWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+    @Override
+    public void onReceive(Context context, Intent intent){
+        super.onReceive(context, intent);
+        if("GetData".equals(intent.getAction())){
+            int widgetID = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            if(widgetID != AppWidgetManager.INVALID_APPWIDGET_ID){
+                var views = new RemoteViews(context.getPackageName(), R.layout.base_widget);
+                var prefs = context.getSharedPreferences("widget_prefs", Context.MODE_PRIVATE);
+                int number = prefs.getInt(widgetID + "", 0);
+                number++;
+                prefs.edit().putInt(widgetID+"", number).apply();
+                views.setTextViewText(R.id.counter, Integer.toString(number));
+                AppWidgetManager.getInstance(context).updateAppWidget(widgetID, views);
+            }
+        }
     }
 }
